cf8c025f7df17359b603e2134acc0990d6e9d7e7
Jon Wehner <jonjwehner@gmail.com>
2022-01-04 08:39:17 -0800

    updated timestring conversion and added more test assertions

--- app/utils.py
+++ app/utils.py
@@ -7,13 +7,13 @@ def convert_datestring(datestring):
     """
     Splits a date string to create a new datetime object
     """
-    datestring = datestring.replace('Z', '')
     split_datetime= datestring.split('T')
 
     date = split_datetime[0].split('-')
     date = [int(el) for el in date]
 
     time = split_datetime[1].split(':')
+    time[2] = time[2][0:2]
     time = [int(el) for el in time]
 
     return datetime(*date, *time)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,9 +6,15 @@ from app.utils import convert_datestring
 
 TEST_DATE = "2020-11-02T14:00:00Z"
 
-def test_datestring_converstion():
+def test_datestring_conversion():
     """
     Tests that the string successfully converted to a datetime object
     """
     result = convert_datestring(TEST_DATE)
     assert isinstance(result, datetime)
+    assert result.year == 2020
+    assert result.month == 11
+    assert result.day == 2
+    assert result.hour == 14
+    assert result.minute == 0
+    assert result.second == 0
